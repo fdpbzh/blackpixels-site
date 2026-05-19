4863eb36d8c3a3cac242e3abc0bb16e646e27fa6
fdpbzh <blackpixels.fr@gmail.com>
2026-05-19 10:51:56 +0200

    Squelette Astro initial - home, about, prestations, FAQ, contact

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://www.blackpixels.fr',
+  trailingSlash: 'ignore',
+  build: {
+    format: 'file',
+  },
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
+});
